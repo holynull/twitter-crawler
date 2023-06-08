94f049112ebb01d810c58675c579c78267917bd3
holynull <holynull@26..com>
2023-06-08 15:34:15 +0800

    add fields

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+all: get_twitter_SwftCoin get_twitter_SWFTBridge get_twitter_SlowMist_Team get_twitter_evilcos get_twitter_whale_alert get_twitter_wublockchain12 get_twitter_list_1449651877751050242
+
 get_twitter_SwftCoin:
 	python get_twitter_data.py -u SwftCoin
 get_twitter_SWFTBridge:
@@ -12,3 +14,7 @@ get_twitter_wublockchain12:
 	python get_twitter_data.py -u wublockchain12
 get_twitter_list_1449651877751050242:
 	python get_twitter_list.py -l 1449651877751050242
+
+final:
+	tar cvzf data.tar ./*.csv
+	rm -rf *.csv
--- get_twitter_data.py
+++ get_twitter_data.py
@@ -47,11 +47,11 @@ def save_tweets_to_csv(tweets, file_name="tweets.csv"):
     with open(file_name, mode='w', encoding='utf-8', newline='') as f:
         writer = csv.writer(f, delimiter=',', quotechar='"',
                             quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(["User", "Tweet", "Time"])
+        writer.writerow(["User", "Tweet", "Time","Retweet","Favorite"])
 
         for tweet in tweets:
             writer.writerow(
-                [tweet.user.screen_name, tweet.full_text, tweet.created_at])
+                [tweet.user.screen_name, tweet.full_text, tweet.created_at,tweet.retweet_count,tweet.favorite_count])
 
 
 if __name__ == "__main__":
